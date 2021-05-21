b035c829e63290441af33ee7dcc1044b7961947d
Quintine <sol.quintine@gmail.com>
2021-05-21 15:49:37 +0200

    I made a main page and I try to let it open a navigation drawer when the upper left button is pressed, but I just can't seem to figure it out. Everytime I run it now, I get an error when I press that button. Do you see what's going wrong? The app also crashes when I try to add an event or show the events on a certain day, but it could just be me.

---
+++ app/src/main/java/com/example/all_chores/MainPage.java
@@ -0,0 +1,78 @@
+package com.example.all_chores;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainPage extends AppCompatActivity {
+
+        DrawerLayout drawerLayout;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.main_page);
+
+            //ViewPager viewPager = findViewById(R.id.view_pager);
+
+            //viewPager.setAdapter(R.id.main_content);
+
+            drawerLayout = findViewById(R.id.drawer_layout);
+
+            Button exit = findViewById(R.id.exit);
+            exit.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    drawerLayout.openDrawer(GravityCompat.START);
+                }
+            });
+
+            Button calendar = findViewById(R.id.btn_calender);
+            calendar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainPage.this, CalendarActivity.class);
+                    startActivity(intent);
+                }
+            });
+
+            Button info = findViewById(R.id.btn_information);
+            info.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainPage.this, ChoresInformation.class);
+                    startActivity(intent);
+                }
+            });
+
+            FloatingActionButton fab = findViewById(R.id.fab);
+
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Snackbar.make(view, "You can view the calendar or information about house chores." +
+                            "A menu opens using the top left button.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
+            });
+        }
+}
